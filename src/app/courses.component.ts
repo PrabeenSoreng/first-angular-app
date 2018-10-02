9e62d4e80a2e03fa37d1d606d2c71eb86374b9f1
Prabeen <prabeensoreng2119@gmail.com>
2018-10-02 12:38:02 +0530

    custom summary pipe added

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,18 +4,25 @@ import { CoursesService } from './courses.service';
     selector: 'courses',
     template: `
             <h2>{{ title }}</h2>
+            {{date | date:'mediumDate'}}
             <ul>
                 <li *ngFor="let course of courses">
                     {{course}}
                 </li>
             </ul>
-            <button class="btn btn-primary">Save</button>
+            {{text | summary:100}} <br />
+            <button class="btn btn-primary" (click)="onSave($event)">Save</button>
         `
 })
 export class CoursesComponent {
     title = "List of courses";
+    date = Date.now();
+    text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
     courses;
     constructor(service: CoursesService) {
         this.courses = service.getCourses();
+    }
+    onSave($event){
+        console.log("Button was clicked", $event);
     }
 }
